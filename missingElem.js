80afcd05bd80acd19c9d0a80e3ae862f17674d6f
Muhammad Khuzaima Ansari <khuzaimaansari364@gmail.com>
2024-08-15 15:37:46 +0500

    Updated logic of missingElem.js

--- missingElem.js
+++ missingElem.js
@@ -8,29 +8,27 @@ function missingElem(arr){
             frquency[arr[i]]++;
         }
         else{
-            frquency[arr[i]] = 1
+            frquency[arr[i]] = 1;
         }
     }
     
     for(let i = 0; i < arr.length; i++){
         if(frquency[arr[i]] === 2){
-            console.log("Repeating: ", arr[i])
+            console.log("Repeating: ", arr[i]);
         }
     }
 
-    for(let i = 0; i < arr.length; i++){
+    for(let i = 1; i < arr.length; i++){
 
-        if(arr[i + 1] === arr[i] + 1){
+        if(arr[i] === arr[i - 1] + 1){
             value = arr[i];
         }
-        else if(arr[i + 1] !== arr[i] + 1){
-            console.log("Missing Element: " , value + 2);
+        else if(arr[i] !== arr[i - 1] + 1){
+            console.log("Missing Element: " , value + 1, "At Index: ", i);
+            i++
         }
-        // if(arr[i + 1] !== arr[i] + 1){
-        //     console.log("Missing: ", arr[i] + 1,"At index: ", i);
-        // }
     }
 }
 
-let array = [1,2,3,4,1,6,7,9];
+let array = [1,2,3,4,1,6,7,7];
 console.log(missingElem(array));
