3d6fc1c3d88c1b8dbd92e1cb4c9b66b294ad2f52
Muhammad Khuzaima Ansari <khuzaimaansari364@gmail.com>
2024-08-20 09:36:10 +0500

    Learned About DOM and its Manuplation

---
+++ domManuplation.js
@@ -0,0 +1,15 @@
+const body = document.body;
+// // body.append("Hello World")
+
+// const div = document.createElement('div');
+
+// // div.innerText = "Hello World";
+
+// body.append(div)
+
+div = document.querySelector('div')
+
+console.log(div.textContent);
+console.log(div.innerText);
+
+console.log(div.setAttribute("id", "div-1"))
